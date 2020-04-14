984e6287f42ea53d6411faf20cd2b3944f72394d
= <hestonw10@gmail.com>
2020-04-14 16:59:49 -0700

    add stuff to limelight subsystem

--- src/main/cpp/subsystems/LimelightSubsystem.cpp
+++ src/main/cpp/subsystems/LimelightSubsystem.cpp
@@ -11,3 +11,28 @@ tx = m_table->GetNumber("tx", PenguinConstants::Limelight::DEFAULT_VALUE);
 ty = m_table->GetNumber("ty", PenguinConstants::Limelight::DEFAULT_VALUE);
 
 }
+
+void Limelight::SetVisionCamMode() {
+  table->PutNumber("ledMode", 3); // force LEDs on
+  table->PutNumber("camMode", 0); // vision processor
+}
+
+void Limelight::SetDriveCamMode() {
+  table->PutNumber("ledMode", 1); // force LEDs off
+  table->PutNumber("camMode", 1); // drive cam (increases exposure, disables vision processing)
+}
+//TODO figure out best way to use the limelight autonomous as subsystem
+// could be setdefault command
+// units::radians_per_second_t LimelightAutonomous::Calculate(units::radian_t currentAngle) {
+//   const LimelightValues vals = m_limelight->GetInfo();
+
+//   return units::radians_per_second_t(
+//     m_pidController.Calculate(vals.tx)
+//   );
+// }
+// }
+// LimelightAutonomous::LimelightAutonomous(Limelight* limelight)
+// :m_limelight{limelight} {
+//     m_pidController.SetSetpoint(0);
+//     m_pidController.SetTolerance(2);
+// }
--- src/main/include/subsystems/LimelightSubsystem.h
+++ src/main/include/subsystems/LimelightSubsystem.h
@@ -5,15 +5,18 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 #include "PenguinUtil.h"
 #include "Constants.h"
+#include "PenguinControl/PIDController.h"
 
 class LimelightSubsystem : public frc2::SubsystemBase {
     public:
     LimelightSubsystem();
     void Periodic() override;
     //TODO find solution to doing the autonomous limelight stuff.
-
+    void SetVisionCamMode();
+    void SetDriveCamMode();
     double tx, ty;
-    
+    PenguinPIDController m_pidController{1, 0, 0}; // curious if this will work.
+    units::radians_per_second_t CalculateRot(units::radian_t currentAngle);
     private:
     std::shared_ptr<NetworkTable> m_table;
 };

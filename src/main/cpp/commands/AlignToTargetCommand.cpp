00824b73b14729a3baa218ca5ad5752ee37983cb
= <hestonw10@gmail.com>
2020-04-15 15:44:11 -0700

    work more on AlignToTargetCommand, pretty sketch

--- src/main/cpp/commands/AlignToTargetCommand.cpp
+++ src/main/cpp/commands/AlignToTargetCommand.cpp
@@ -1,5 +1,6 @@
 #include "commands/AlignToTargetCommand.h"
 #include "RobotContainer.h"
+#include <cmath>
 
 AlignToTargetCommand::AlignToTargetCommand(LimelightSubsystem* limelight, DriveSubsystem* drivetrain)
 :m_limelight{limelight},
@@ -19,19 +20,34 @@ void AlignToTargetCommand::Execute() {
   m_limelight->TurnLEDOn();
   m_limelight->SetAsVisionProcessor();
   //TODO: get this good.
-//   //m_timer.Delay(0.3);
-//   double scalar = 0.15;
-//   double forward = m_leftJoystick.GetRawAxis(1) * scalar;
-//   double strafe;
-//   //TODO make this section of code not absolute garbage.
-//   if(m_limelight->HasAnyValidTargets() && fabs(m_rightJoystick.GetRawAxis(0);) < 0.75) {
-//     strafe = controller.Calculate(units::radian_t(m_limelight.horizontalOffsetAngle), dt); //TODO see if this needs to be passed through deadband
-//   } else {
-//       value = PenguinUtil::deadband(m_rightJoystick.GetRawAxis(0));
-//       strafe = std::copy_sign(value * value, value)               //trying simple deadband like 2910, may not work out
-//   }
-  
-  
+  //m_timer.Delay(0.3);
+  double scalar = 0.15;
+  double strafe;
+  double forward = m_leftJoystick.GetRawAxis(1);
+  //forward *= -1;  //TODO: invert forward or not?
+  forward = PenguinUtil::smartDeadband(forward, -0.2, 0.16) * scalar;
+      
+      forward = copysign(pow(forward, 2), forward);
+      double strafe = m_rightJoystick.GetRawAxis(0);
+      //strafe = PenguinUtil::smartDeadband(strafe, -0.18, 0.15);
+      //strafe *= -1;
+      //strafe = copysign(pow(strafe, 2), strafe);
+      double rotation = m_leftJoystick.GetRawAxis(2);
+      // SD::PutNumber("rot raw", rotation);
+      
+      rotation = copysign(pow(rotation, 2), rotation);
+  //TODO make this section of code not absolute garbage.
+  if(m_limelight->HasAnyValidTargets() && fabs(PenguinUtil::deadband((m_rightJoystick.GetRawAxis(0)), 0.025) < 0.75)){
+    strafe = controller.Calculate(units::radian_t(m_limelight.horizontalOffsetAngle), dt); 
+  } else {
+      value = PenguinUtil::deadband(m_rightJoystick.GetRawAxis(0));
+      strafe = -PenguinUtil::deadband(strafe);
+      strafe = copysign(strafe * strafe, strafe);            
+  }
+  double rotation = m_leftJoystick.GetRawAxis(2);
+  rotation *= -1;
+  rotation = PenguinUtil::deadband(rotation, 0.025);
+  rotation = copysign(rotation * rotation, rotation);
   
 }
 bool AlignToTargetCommand::IsFinished() {
@@ -47,5 +63,9 @@ bool AlignToTargetCommand::IsFinished() {
   
     
 //   }
+//no end condition because this command will be run by a button.
   return false;
+}
+void AlignToTargetCommand::End(bool interrupted) {
+    m_limelight->SetAsDriverCamera();
 }
---
+++ src/main/include/DriveUtil/HolonomicFollower.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "DriveUtil/Vector2d.h"
+//TODO encapsulate data when this is not a hacked together solution.
+struct HolonomicDriveSignal {
+HolonomicDriveSignal(Vector2d translation, double rotation, bool fieldOriented);
+Vector2d m_translation;
+double m_rotation;
+bool m_fieldOriented;
+Vector2 GetTranslation();
+double GetRotation();
+bool IsFieldOriented();
+};
+class HolonomicFeedforward {
+ public:
+  HolonomicFeedforward(DrivetrainFeedforward forwardConstants, DrivetrainFeedforward strafeConstants);
+  HolonomicFeedforward(DrivetrainFeedforward translationConstants)
+  Vector2d CalculateFeedforward(Vector2d velocity, Vector2d acceleration);
+  DrivetrainFeedforward GetForwardConstants();
+  DrivetrainFeedforward GetStrafeConstants();
+ private:
+  DrivetrainFeedforward forwardConstants;
+  DrivetrainFeedforward strafeConstants;
+
+};
+class DrivetrainFeedforward {
+ public:
+  DrivetrainFeedforward(double velocityConstant, double accelerationConstant, double staticConstant);
+  double CalculateFeedforward(double velocity, double acceleration);
+  double GetVelocityConstant();
+  double GetAccelerationConstant();
+  double GetStaticConstant();
+ private:
+  double velocityConstant;
+  double accelerationConstant;
+  double staticConstant;
+};
